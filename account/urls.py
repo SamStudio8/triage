406c6c346b8da5fa7dac749aadb93c7217ed2af3
Sam Nicholls <sam.n@studio8media.co.uk>
2013-10-28 18:21:29 +0000

    Auth links

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 from django.contrib.auth.views import login, logout
 
 urlpatterns = patterns('',
-    url(r'^login/$', login, {"template_name":"account/login.html"}, name="login"),
-    url(r'^logout/$', logout, {'next_page': '/'}, name="logout"),
+    url(r'^login/$', login, {'template_name':'account/login.html'}, name='login'),
+    url(r'^logout/$', logout, {'next_page': '/'}, name='logout'),
 )
--- task/urls.py
+++ task/urls.py
@@ -4,6 +4,5 @@ import task.views as TaskViews
 urlpatterns = patterns('',
     url(r'^add/(?P<tasklist_id>\w+)/$', TaskViews.add_task, name="add_task"),
     url(r'^edit/(?P<task_id>\w+)/$', TaskViews.edit_task, name="edit_task"),
-
     url(r'^addlist/$', TaskViews.add_tasklist, name="add_tasklist"),
 )
