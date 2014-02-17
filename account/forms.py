8381018524f0959f8d98f025fbae547dec975a25
Sam Nicholls <sam.n@studio8media.co.uk>
2014-02-17 14:00:56 +0000

    Crispify reg form

--- account/forms.py
+++ account/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from django.contrib.auth.models import User
 from django.utils.html import escape
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Div, Fieldset, Submit
+from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
+
 class RegistrationForm(forms.Form):
     username = forms.CharField(max_length=30)
     first_name = forms.CharField(max_length=30)
@@ -9,6 +13,38 @@ class RegistrationForm(forms.Form):
     email = forms.EmailField()
     password1 = forms.CharField(widget=forms.PasswordInput(), label="Password")
     password2 = forms.CharField(widget=forms.PasswordInput(), label="Confirm password")
+
+    def __init__(self, user_id, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+
+        # django-crispy-forms
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-lg-3'
+        self.helper.field_class = 'col-lg-9'
+        self.helper.layout = Layout(
+            Div(
+                Div(
+                    Fieldset('Basic',
+                        'username',
+                        'first_name',
+                        'last_name',
+                        'email',
+                    ),
+                    css_class="col-lg-6"),
+                Div(
+                    Fieldset('Password',
+                        'password1',
+                        'password2',
+                    ),
+                    css_class="col-lg-6"
+                ),
+                css_class="row"
+            ),
+            FormActions(
+                Submit('save', 'Save'),
+            )
+        )
 
     def clean_username(self):
         username = self.cleaned_data['username']
